39d4d12a273c6e1a6c003574bbe16f0bd8309a7c
Bigfire3 <98695515+Bigfire3@users.noreply.github.com>
2024-07-08 23:49:49 +0200

    Update TutorialManager.cs
    Things added today:
    - Intro
    - first explained jump

--- TutorialManager.cs
+++ TutorialManager.cs
@@ -8,13 +8,16 @@ public class TutorialManager : MonoBehaviour
     public GameObject playerObject;
     PlayerManagerTutorial playerManager;
     TypeWriting typeWriter;
-    public Animation intructionAnimator;
+    public Animation intructionAnimation;
+    int jumpsDone = 0;
 
     public enum State
     {
-        start,
-        text_0,
-        touch_0,
+        start, // intro IEnumerator
+        waitForAim_0,
+        waitForTouchEnd_0,
+        waitForMoreJumps,
+        waitForPassObstacles,
         text_1,
         touch_1
     }
@@ -24,7 +27,7 @@ public class TutorialManager : MonoBehaviour
     {
         playerManager = playerObject.GetComponent<PlayerManagerTutorial>();
         typeWriter = GetComponent<TypeWriting>();
-        typeWriter.OnTextTyped += OnTextTyped;
+        //typeWriter.OnTextTyped += OnTextTyped;
 
         StartCoroutine(TutorialStart());
     }
@@ -32,12 +35,37 @@ public class TutorialManager : MonoBehaviour
     {
         switch (state)
         {
-            case State.touch_0:
-                
+            case State.waitForAim_0:
+                playerManager.TouchMethod(
+                    doInTouchPhaseAim: () =>
+                    {
+                        typeWriter.StartTypingText("Perfectly\nNow let go to jump");
+                        state = State.waitForTouchEnd_0;
+                    });
                 break;
             
-            case State.touch_1:
-                Debug.Log("touch_1");
+            case State.waitForTouchEnd_0:
+                playerManager.TouchMethod(doInTouchPhaseEndedWithVelocity: () =>
+                {
+                    typeWriter.StartTypingText("Do more Jumps");
+                    intructionAnimation.gameObject.SetActive(false);
+                    state++;
+                });
+                break;
+            case State.waitForMoreJumps:
+                playerManager.TouchMethod(doInTouchPhaseEndedWithVelocity: () =>
+                {
+                    jumpsDone++;
+                });
+                if (jumpsDone > 2)
+                {
+                    typeWriter.StartTypingText("Don't collide with the obstacles");
+                    state++;
+                }
+                break;
+            case State.waitForPassObstacles:
+                Debug.Log("Spawn enemies");
+                playerManager.TouchMethod();
                 break;
         }
     }
@@ -50,14 +78,14 @@ public class TutorialManager : MonoBehaviour
     {
         instructionPanelObject.LeanMoveLocalY(-1170, 1).setEaseOutElastic();
         yield return new WaitForSeconds(1);
-        typeWriter.startTypingText("You play this ball", mute: true, invokeEvent: false);
+        typeWriter.StartTypingText("You play this ball", mute: true);
         yield return new WaitForSeconds(2);
         playerObject.SetActive(true);
         yield return new WaitForSeconds(1);
-        intructionAnimator.gameObject.SetActive(true);
-        intructionAnimator.Play("FingerDrag_0");
-        typeWriter.startTypingText("Tap and drag to aim", mute: true, invokeEvent: false);
-        state = State.text_0;
+        intructionAnimation.gameObject.SetActive(true);
+        intructionAnimation.Play("FingerDrag_0");
+        typeWriter.StartTypingText("Tap and drag to aim", mute: true);
+        state = State.waitForAim_0;
     }
 
     public void OnCancelButtonPress()
